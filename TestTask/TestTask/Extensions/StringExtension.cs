8614f2e596c29ccee7bc26c29919f8f7a61e501f
Yaroslav <zherebetskiy00@gmail.com>
2019-05-13 15:51:25 +0300

    added extension for string and implemented "SWAP" and "REPLACE"

---
+++ TestTask/TestTask/Extensions/StringExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace TestTask.Extensions
+{
+    public static class StringExtension
+    {
+        public static string Swap(this string value, int firstIndex, int lastIndex)
+        {
+            StringBuilder stringBuilder = new StringBuilder(value);
+            stringBuilder[firstIndex] = value[lastIndex];
+            stringBuilder[lastIndex] = value[firstIndex];
+
+            return stringBuilder.ToString();
+        }
+    }
+}
--- TestTask/TestTask/Program.cs
+++ TestTask/TestTask/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using TestTask.Extensions;
 
 namespace TestTask
 {
@@ -30,9 +32,16 @@ namespace TestTask
             {
                 if (S[i] != T[i])
                 {
+                    if (S[i] == T[i + 1] && S[i + 1] == T[i])
+                    {
+                        S = S.Swap(i, i + 1);
+                        return string.Equals(S, T) ? $"SWAP {S[i + 1]} {S[i]}" : "IMPOSSIBLE";
+                    }
 
-
-                    return "REPLACE or SWAP ";
+                    StringBuilder stringBuilder = new StringBuilder(S);
+                    stringBuilder[i] = T[i];
+                    S = stringBuilder.ToString();
+                    return string.Equals(S, T) ? $"REPLACE {S[i]} {T[i]}" : "IMPOSSIBLE";
                 }
             }
 
@@ -42,6 +51,9 @@ namespace TestTask
         static void Main(string[] args)
         {            
             Console.WriteLine(Solution("nice", "niece"));
+            Console.WriteLine(Solution("test", "tent"));
+            Console.WriteLine(Solution("form", "from"));
+            Console.WriteLine(Solution("o", "odd"));
             Console.ReadKey();
         }
     }
