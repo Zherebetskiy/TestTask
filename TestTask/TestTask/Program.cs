60db9737d4c31ecd6330dbce31aeb0c341dae3f1
Yaroslav <zherebetskiy00@gmail.com>
2019-05-13 16:08:13 +0300

    separated Solution function and implemented "EQUAL"

--- TestTask/TestTask/Program.cs
+++ TestTask/TestTask/Program.cs
@@ -1,59 +1,16 @@
 ﻿using System;
-using System.Text;
-using TestTask.Extensions;
 
 namespace TestTask
 {
     class Program
-    {
-        public static string Solution(string S, string T)
-        {
-            var result = string.Empty;
-
-            if (Math.Abs(S.Length - T.Length) > 1)
-            {
-                return "IMPOSSIBLE";
-            }
-
-            if (Math.Abs(S.Length - T.Length) == 1)
-            {
-                for (int i = 0; i < S.Length; i++)
-                {
-                    if (S[i] != T[i])
-                    {
-                       S = S.Insert(i, T[i].ToString());
-
-                        return string.Equals(S,T) ? $"INSERT {T[i]}" : "IMPOSSIBLE";
-                    }
-                }
-            }
-
-            for (int i = 0; i < S.Length; i++)
-            {
-                if (S[i] != T[i])
-                {
-                    if (S[i] == T[i + 1] && S[i + 1] == T[i])
-                    {
-                        S = S.Swap(i, i + 1);
-                        return string.Equals(S, T) ? $"SWAP {S[i + 1]} {S[i]}" : "IMPOSSIBLE";
-                    }
-
-                    StringBuilder stringBuilder = new StringBuilder(S);
-                    stringBuilder[i] = T[i];
-                    S = stringBuilder.ToString();
-                    return string.Equals(S, T) ? $"REPLACE {S[i]} {T[i]}" : "IMPOSSIBLE";
-                }
-            }
-
-            return result;
-        }
-
+    {   
         static void Main(string[] args)
         {            
-            Console.WriteLine(Solution("nice", "niece"));
-            Console.WriteLine(Solution("test", "tent"));
-            Console.WriteLine(Solution("form", "from"));
-            Console.WriteLine(Solution("o", "odd"));
+            Console.WriteLine(StringSolution.Solution("nice", "niece"));
+            Console.WriteLine(StringSolution.Solution("test", "tent"));
+            Console.WriteLine(StringSolution.Solution("form", "from"));
+            Console.WriteLine(StringSolution.Solution("o", "odd"));
+            Console.WriteLine(StringSolution.Solution("EQUAL", "EQUAL"));
             Console.ReadKey();
         }
     }
---
+++ TestTask/TestTask/StringSolution.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using TestTask.Extensions;
+
+namespace TestTask
+{
+    public static class StringSolution
+    {
+        public static string Solution(string S, string T)
+        {
+            var result = string.Empty;
+
+            if (string.Equals(S,T))
+            {
+                return "EQUAL";
+            }
+
+            if (Math.Abs(S.Length - T.Length) == 1)
+            {
+                for (int i = 0; i < S.Length; i++)
+                {
+                    if (S[i] != T[i])
+                    {
+                        S = S.Insert(i, T[i].ToString());
+
+                        return string.Equals(S, T) ? $"INSERT {T[i]}" : "IMPOSSIBLE";
+                    }
+                }
+            }
+
+            if (S.Length == T.Length)
+            {
+                for (int i = 0; i < S.Length; i++)
+                {
+                    if (S[i] != T[i])
+                    {
+                        if (S[i] == T[i + 1] && S[i + 1] == T[i])
+                        {
+                            S = S.Swap(i, i + 1);
+
+                            return string.Equals(S, T) ? $"SWAP {S[i + 1]} {S[i]}" : "IMPOSSIBLE";
+                        }
+
+                        StringBuilder stringBuilder = new StringBuilder(S);
+                        stringBuilder[i] = T[i];
+                        var temp = S[i];
+                        S = stringBuilder.ToString();
+
+                        return string.Equals(S, T) ? $"REPLACE {temp} {T[i]}" : "IMPOSSIBLE";
+                    }
+                }
+            }
+
+            return "IMPOSSIBLE";
+        }
+    }
+}
