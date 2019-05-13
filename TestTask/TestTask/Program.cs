c671bc8fa2d83eab8e87b6dcea4afd2af26b5ba0
Yaroslav <zherebetskiy00@gmail.com>
2019-05-13 15:22:07 +0300

    implemented "INSERT" logic

--- TestTask/TestTask/Program.cs
+++ TestTask/TestTask/Program.cs
@@ -4,9 +4,45 @@ namespace TestTask
 {
     class Program
     {
-        static void Main(string[] args)
+        public static string Solution(string S, string T)
         {
-            Console.WriteLine("Hello World!");
+            var result = string.Empty;
+
+            if (Math.Abs(S.Length - T.Length) > 1)
+            {
+                return "IMPOSSIBLE";
+            }
+
+            if (Math.Abs(S.Length - T.Length) == 1)
+            {
+                for (int i = 0; i < S.Length; i++)
+                {
+                    if (S[i] != T[i])
+                    {
+                       S = S.Insert(i, T[i].ToString());
+
+                        return string.Equals(S,T) ? $"INSERT {T[i]}" : "IMPOSSIBLE";
+                    }
+                }
+            }
+
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (S[i] != T[i])
+                {
+
+
+                    return "REPLACE or SWAP ";
+                }
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {            
+            Console.WriteLine(Solution("nice", "niece"));
+            Console.ReadKey();
         }
     }
 }
